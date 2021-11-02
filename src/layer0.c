c4e36e5c5c47b95bd224882af63e806564d77c4d
André Wagner <WagnerAndre@JohnDeere.com>
2021-11-02 09:45:52 -0300

    .

--- src/layer0.c
+++ src/layer0.c
@@ -4,12 +4,12 @@ FFatResult f_raw_read(FFat* f)
 {
     if (f->F_RAWSEC >= total_sectors())
         return F_IO_ERROR;
-    return raw_read(f->F_RAWSEC, f->buffer);
+    return raw_read(f->F_RAWSEC, f->buffer) ? F_OK : F_IO_ERROR;
 }
 
 FFatResult f_raw_write(FFat* f)
 {
     if (f->F_RAWSEC >= total_sectors())
         return F_IO_ERROR;
-    return raw_write(f->F_RAWSEC, f->buffer);
+    return raw_write(f->F_RAWSEC, f->buffer) ? F_OK : F_IO_ERROR;
 }
--- tests/test.c
+++ tests/test.c
@@ -1,13 +1,37 @@
 #include "test.h"
 
+#include <string.h>
+
+#include "image.h"
 #include "scenario.h"
+
+FDateTime date_time = {};
+FFatResult last_result;
+
+#define X_OK(expr) { last_result = (expr); if (last_result != F_OK) return false; }
+#define ASSERT(expr) { if (!(expr)) return false; }
 
 bool test_raw_sector(FFat* f)
 {
+    f->F_RAWSEC = 0xaf;
+    
+    X_OK(ffat_op(f, F_READ_RAW, date_time))
+    ASSERT(f->buffer[0] == 0xaf)
+    ASSERT(f->buffer[511] == 0xaf)
+    
+    memset(f->buffer, 0x33, 512);
+    X_OK(ffat_op(f, F_WRITE_RAW, date_time))
+    memset(f->buffer, 0, 512);
+    
+    X_OK(ffat_op(f, F_READ_RAW, date_time))
+    ASSERT(f->buffer[0] == 0x33)
+    ASSERT(f->buffer[511] == 0x33)
+    ASSERT(img_data[0xaf * SECTOR_SZ] == 0x33);
+    
     return true;
 }
 
-const Test _test_list[] = {
+static const Test _test_list[] = {
         { "Raw sector access", { scenario_raw_sectors, NULL }, test_raw_sector },
         { NULL, {}, NULL },
 };
